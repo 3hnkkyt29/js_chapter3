d3fb332561447e62bc7db291bb63858a359fc61b
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-26 01:21:39 +0000

    finish 3-11

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -47,6 +47,7 @@ color.addEventListener('input', colorBg);
 */
 
 // 3-10
+/*
 const colorBg = () => {
  // 選択した色を背景色に設定
  document.body.style.backgroundColor = color.value;
@@ -55,5 +56,22 @@ const colorBg = () => {
 }
 // カラーピッカーが変更されたらcolorBgを発動
 color.addEventListener('input', colorBg);
-
+*/
 // 要素.style.CSSのプロパティ名（ハイフンが入る場合はキャメルケースで書く） = 値;
+
+// 3-11
+const colorBg = () => {
+ // 選択した色を背景色に設定
+ document.body.style.backgroundColor = color.value;
+ // カラーコードを表示
+ if (color.value === '#ffffff') {
+  text.textContent = `カラーコード：${color.value}(white)`;
+ } else if (color.value === '#000000') {
+  text.textContent = `カラーコード：${color.value}(black)`;
+ }
+ else {
+  text.textContent = `カラーコード：${color.value}`;
+ }
+}
+ // カラーピッカーが変更されたらcolorBgを発動
+color.addEventListener('input', colorBg);
